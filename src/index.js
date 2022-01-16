27486a273b600217c7add25878db580363d59ccb
“Daysha <+>
2022-01-15 19:16:11 -0800

    commit tree 1abdec39633c7d70880762b2e984de779b0c4826

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const todoForm = document.getElementById('create-task-form');
+  todoForm.addEventListener('submit', handleSubmit);
+
 });
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const taskList = document.getElementById('tasks');
+  const task = document.createElement('li');
+  task.textContent = event.target.description.value;
+  taskList.append(task);
+
+}
